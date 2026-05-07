9dd408f015f48111c6d8b1955963f8fa1c918132
IraMira333 <irinat333@gmail.com>
2026-05-07 16:12:17 +0300

    Update WhyWe.tsx

--- src/components/main/whyWe/WhyWe.tsx
+++ src/components/main/whyWe/WhyWe.tsx
@@ -4,7 +4,7 @@ import { LocaleType } from "@/src/types/LocaleTypes";
 
 import { dezTelegram } from "../../assets/contacts";
 import { cubeList } from "../../assets/dataLists";
-import { MaskLink } from "../../shared/MaskLink";
+import { GlowLink } from "../../shared/GlowLink";
 import { Subtitle } from "../../shared/Subtitle";
 import { CubeSlider } from "./CubeSlider";
 
@@ -17,13 +17,9 @@ export const WhyWe = () => {
     <section className="casual-container relative min-h-[calc(100vh-77px)] bg-[url('/bgimages/bg-whywe.png')] bg-cover bg-center pt-[calc((100vh-77px)/4)] pb-12">
       <Subtitle text={t("whyWeTitle")} className="mb-12" />
       <CubeSlider list={whyWelist} />
-      <MaskLink
-        noArrow
-        white
-        text={t("orderProject")}
-        link={dezTelegram}
-        className="mx-auto w-full max-w-80"
-      />
+      <div className="mx-auto w-full max-w-80">
+        <GlowLink link={dezTelegram} dark text={t("orderProject")} className="mx-auto" />
+      </div>
     </section>
   );
 };
